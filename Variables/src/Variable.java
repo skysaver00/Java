fa51dcbfb8a09e3fc47df091a0a41acb8c857098
Young Woo Kim <skysaver00@ajou.ac.kr>
2021-01-01 20:45:02 +0900

    Variable

---
+++ Variables/src/Variable.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber); //sout하고 탭해면 바로 나옴
+        System.out.println("myFirstNumber");
+    }
+}
