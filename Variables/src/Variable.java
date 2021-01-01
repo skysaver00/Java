2d749ffa772f2ba27f63b7f3b5ce18a3d24c6184
Young Woo Kim <skysaver00@ajou.ac.kr>
2021-01-01 20:45:53 +0900

    Variable

--- Variables/src/Variable.java
+++ Variables/src/Variable.java
@@ -3,7 +3,7 @@ import java.sql.SQLOutput;
 public class Variable {
 
     public static void main(String[] args) {
-        int myFirstNumber = 5;
+        int myFirstNumber = 10 + 5;
 
         System.out.println(myFirstNumber); //sout하고 탭해면 바로 나옴
         System.out.println("myFirstNumber");
