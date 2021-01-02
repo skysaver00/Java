8eef6318a90f0775b9d3e1490162b33c154fe8ff
skysaver00 <skysaver00@naver.com>
2021-01-02 11:46:42 +0900

    Variable

--- Variables/src/Variable.java
+++ Variables/src/Variable.java
@@ -5,9 +5,11 @@ public class Variable {
     public static void main(String[] args) {
         int myFirstNumber = 10;
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
 
-        System.out.println(myFirstNumber + mySecondNumber + myThirdNumber); //sout하고 탭해면 바로 나옴
+        System.out.println(myTotal + ", " + myLastOne); //sout하고 탭해면 바로 나옴
         System.out.println("myFirstNumber");
     }
 }
