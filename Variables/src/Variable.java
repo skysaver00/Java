13b47cd6e775194a80c13857a2070ab2e72b00df
skysaver00 <skysaver00@naver.com>
2021-01-02 11:39:31 +0900

    Variable

--- Variables/src/Variable.java
+++ Variables/src/Variable.java
@@ -3,9 +3,11 @@ import java.sql.SQLOutput;
 public class Variable {
 
     public static void main(String[] args) {
-        int myFirstNumber = (10 + 5) + (2 * 10);
+        int myFirstNumber = 10;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
 
-        System.out.println(myFirstNumber); //sout하고 탭해면 바로 나옴
+        System.out.println(myFirstNumber + mySecondNumber + myThirdNumber); //sout하고 탭해면 바로 나옴
         System.out.println("myFirstNumber");
     }
 }
