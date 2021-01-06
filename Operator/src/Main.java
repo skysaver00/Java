17120ca5348625dcf64e622c5c52c74bb57344fd
skysaver00 <skysaver00@naver.com>
2021-01-06 15:18:45 +0900

    2020-01-06

---
+++ Operator/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 10 + 15;
+        int minus = 20 - 49;
+        int mul = 15 * 12;
+        int dvd = 12 / 5;
+
+        System.out.println(sum + " " + minus + " " + mul + " " + dvd);
+
+        //당연히 사칙연산도 가능
+        int cal = 15 * 12 - 15 * 10;
+        System.out.println(cal);
+
+        cal = 10 * ((10 + 15) / (5) + 8);
+        System.out.println(cal);
+
+        cal *= cal;
+        System.out.println(cal);
+
+        int remain = 29 % 6;
+        System.out.println(remain);
+    }
+}
