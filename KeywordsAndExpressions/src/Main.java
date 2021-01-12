138d1786bd93c9ae5e554a92e06f5374d00f1500
skysaver00 <skysaver00@naver.com>
2021-01-12 16:05:08 +0900

    2021-01-12

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double kilometer = (100 * 1.609344);
+
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+        
+        int score = 100;
+        if(score > 99) {
+            System.out.println("You get the high score!");
+            score = 0;
+        }
+    }
+}
