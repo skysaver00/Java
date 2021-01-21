cede13ecc55778906192647b2bc5b400ed098f19
skysaver00 <skysaver00@naver.com>
2021-01-21 23:53:26 +0900

    2021-01-21

--- MinuteToYearsAndDays/src/MinuteToYearsAndDays.java
+++ MinuteToYearsAndDays/src/MinuteToYearsAndDays.java
@@ -1,6 +1,9 @@
 public class MinuteToYearsAndDays {
     public static void printYearsAndDays(long minutes) {
-        if(minutes < 0) System.out.println("Invalid Value");
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
         int days = (int)(minutes / (60 * 24));
         int years = days / 365;
         days = days % 365;
