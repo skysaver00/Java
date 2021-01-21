998306fdbd572c99f29ba85b49dfebdfe880d009
skysaver00 <skysaver00@naver.com>
2021-01-21 11:15:07 +0900

    2021-01-21

---
+++ SecondsAndMinutes/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(40000));
+        System.out.println(getDurationString(82, 30));
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if((minutes < 0) || (seconds < 0 || seconds > 60)) return "Invalid Value";
+
+        int realhour = minutes / 60;
+        int realminute = minutes % 60;
+
+        return realhour + "h " + realminute + "m " + seconds + "s";
+    }
+
+    public static String getDurationString(int seconds) {
+        if(seconds < 0) return "Invalid Values";
+
+        int min = seconds / 60;
+        int sec = seconds % 60;
+
+        return getDurationString(min, sec);
+    }
+}
