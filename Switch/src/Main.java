1d859ea4fa1df7a4be345aba0812f41adcb4bb2f
skysaver00 <skysaver00@naver.com>
2021-01-22 20:46:18 +0900

    2021-01-22

---
+++ Switch/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int switchValue = 1;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            default: //any other cases not above
+                System.out.println("Was not 1 nor 2");
+                break;
+        }
+
+        //more code here!
+    }
+}
