77cc61fb8a0334a16eeded964c1629b9c9456255
skysaver00 <skysaver00@naver.com>
2021-01-22 20:49:27 +0900

    2021-01-22

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int switchValue = 1;
+        int switchValue = 3;
 
         switch(switchValue) {
             case 1:
@@ -9,6 +9,11 @@ public class Main {
 
             case 2:
                 System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5: case 6:
+                System.out.println("Value was a 3 , 4 , 5 or 6");
+                System.out.println("Actually it was a " + switchValue);
                 break;
 
             default: //any other cases not above
