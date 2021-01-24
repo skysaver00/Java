8e2813be50eafaa4b21ab9a1d26953ce57d32cfa
skysaver00 <skysaver00@naver.com>
2021-01-24 16:53:29 +0900

    2021-01-24

--- ForStatement/src/Main.java
+++ ForStatement/src/Main.java
@@ -42,7 +42,8 @@ public class Main {
     public static boolean isPrime(int n) {
         if(n == 1) return false;
 
-        for(int i = 2; i <= n / 2; i++) {
+        //사실 여기서 n / 2보다 Math.sqrt(n)을 쓰는게 더 좋다.
+        for(int i = 2; i <= (long) Math.sqrt(n); i++) {
             if(n % i == 0) {
                 return false;
             }
