0b84cc709daa53796e9c2098288e0659be3468a3
skysaver00 <skysaver00@naver.com>
2021-01-24 16:51:22 +0900

    2021-01-24

--- ForStatement/src/Main.java
+++ ForStatement/src/Main.java
@@ -11,9 +11,43 @@ public class Main {
         for(int i = 0 ; i < 5; i++) {
             System.out.println("Loop " + i + " Hello!");
         }
+
+        System.out.println("************ > _ < ************");
+        for(int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, (double) i)));
+        }
+
+        System.out.println("************ > _ < ************");
+        for(int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, (double) i)));
+        }
+
+        int count = 0;
+        for(int i = 10; i < 500; i++) {
+            if(isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number.");
+                if(count == 300) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 1) return false;
+
+        for(int i = 2; i <= n / 2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
