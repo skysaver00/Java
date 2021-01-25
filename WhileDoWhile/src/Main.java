0d9afe2df3ed0b7c90a189ca962f96b764accfa7
skysaver00 <skysaver00@naver.com>
2021-01-25 11:24:00 +0900

    2021-01-25

---
+++ WhileDoWhile/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 1;
+        while(count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        for(count = 1; count != 6; count++) {
+            System.out.println("Count value is " + count);
+        }
+
+        count = 1;
+        while(true) {
+            if(count == 6) break;
+            System.out.println("Count value is " + count);
+
+            count++;
+        }
+    }
+}
