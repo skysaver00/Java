b90b210252298f4951e178c1c3563979e3028028
skysaver00 <skysaver00@naver.com>
2021-01-25 11:40:28 +0900

    2021-01-25

--- WhileDoWhile/src/Main.java
+++ WhileDoWhile/src/Main.java
@@ -18,5 +18,34 @@ public class Main {
 
             count++;
         }
+
+        count = 1;
+        do {
+            System.out.println("Count value is " + count);
+            count++;
+        }while(count != 6);
+
+        int number = 4;
+        do {
+            if(isEvenNumber(number) == true) System.out.println(number + " is Even!");
+            else System.out.println(number + " is odd...");
+
+            number++;
+        }while(number <= 100);
+
+        int num2 = 1;
+        int count2 = 0;
+        while(count2 != 5) {
+            if(isEvenNumber(num2) == true) {
+                System.out.println(num2 + " is even. count: " + count2);
+                count2++;
+            }
+            num2++;
+        }
+    }
+
+    public static boolean isEvenNumber(int number) {
+        if((number % 2 == 0)) return true;
+        else return false;
     }
 }
