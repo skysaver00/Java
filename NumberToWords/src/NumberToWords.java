ec5b7989438cbff43a710d4ace63404597e582bf
skysaver00 <skysaver00@naver.com>
2021-01-28 11:53:59 +0900

    2021-01-28

---
+++ NumberToWords/src/NumberToWords.java
@@ -0,0 +1,58 @@
+import java.sql.SQLOutput;
+
+public class NumberToWords {
+    public static int reverse(int number) {
+        int reverse = 0;
+
+        while(number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+
+            number /= 10;
+        }
+
+        return reverse;
+    }
+
+    public static int getDigitCount(int number) {
+        int count = 0;
+        if(number < 0) return -1;
+
+        do {
+            count++;
+            number /= 10;
+        } while(number != 0);
+
+        return count;
+    }
+
+    public static void numberToWords(int number) {
+        int count = getDigitCount(number);
+
+        if(number < 0) System.out.println("Invalid Value");
+
+        while(count > 0) {
+            int digit = number % 10;
+
+            if(digit == 0) System.out.println("Zero");
+            else if(digit == 1) System.out.println("One");
+            else if(digit == 2) System.out.println("Two");
+            else if(digit == 3) System.out.println("Three");
+            else if(digit == 4) System.out.println("Four");
+            else if(digit == 5) System.out.println("Five");
+            else if(digit == 6) System.out.println("Six");
+            else if(digit == 7) System.out.println("Seven");
+            else if(digit == 8) System.out.println("Eight");
+            else System.out.println("Nine");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDigitCount(0));
+        System.out.println(getDigitCount(123));
+        System.out.println(getDigitCount(-12));
+        System.out.println(getDigitCount(5200));
+
+        System.out.println();
+    }
+}
