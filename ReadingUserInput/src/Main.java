7e6eb6a14a18b7c749991c795cc2893a9092282e
skysaver00 <skysaver00@naver.com>
2021-02-03 20:06:51 +0900

    2021-02-03

---
+++ ReadingUserInput/src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = scan.nextLine();
+        System.out.print("Enter Your Year of Birth: ");
+        int yearofbirth = scan.nextInt();
+
+        System.out.println("Your name is " + name);
+        System.out.println("Your Birth Year is " + yearofbirth);
+        System.out.println("You are " + (2021 - yearofbirth) + " years old.");
+        scan.close();
+    }
+}
