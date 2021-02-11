e267fefd5ff8dc294505d27c365db97bc2b1e613
skysaver00 <skysaver00@naver.com>
2021-02-11 12:25:00 +0900

    2021-02-11

--- Constructors/src/BankAccount.java
+++ Constructors/src/BankAccount.java
@@ -5,6 +5,31 @@ public class BankAccount {
     private String Email;
     private String phoneNumber;
 
+    //생성자는 클래스로부터 객체를 생성할때 호출되어 객체의 초기화를 담당한다.
+    //인스턴스 변수의 초기화 작업에 주로 사용되고, 인스턴스가 생성될때 실행되어야
+    //할 작업을 위해서도 사용된다.
+
+    //객체의 기본 디폴트 값을 만드는데 생성자가 아주 요긴하게 쓰인다.
+    //생성자의 이름은 항상 클래스의 이름과 같다. 그리고 리턴값이 존재하지 않는다.
+    //생성자도 오버로딩이 가능하다. 따라서 하나의 클래스에 여러개의 생성자가 존재 가능
+    //생성자도 메소드다. 그렇지만 모든 생성자는 리턴값이 없으므로 void를 생략 가능하다.
+
+    //https://programmer-seva.tistory.com/79
+    public BankAccount() {
+        this("00000000", 0.00000, "Default", "aa@a.a.a", "00-000-0000-00");
+        System.out.println("Empty Constructor Called!");
+    }
+
+    public BankAccount(String Number, double balance, String Name, String Email, String phoneNumber) {
+        this.accountNumber = Number;
+        this.balance = balance;
+        this.customerName = Name;
+        this.Email = Email;
+        this.phoneNumber = phoneNumber;
+
+        System.out.println("Account information added.");
+    }
+
     public void setAccountNumber(String Account) {
         this.accountNumber = Account;
     }
--- Constructors/src/Main.java
+++ Constructors/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        BankAccount bobsAccount = new BankAccount();
 
+        BankAccount samsAccount = new BankAccount("3857391", 50.00, "Sam", "sam77@gmail.com", "01-294-2938-29");
+        System.out.println(samsAccount.getAccountNumber());
+        System.out.println(samsAccount.getBalance());
     }
 }
