c472a7af3bf39e60e738a05cfc4d014a7bd93ade
skysaver00 <skysaver00@naver.com>
2021-02-13 16:49:21 +0900

    2021-02-13

---
+++ ComplexOperations/src/ComplexNumber.java
@@ -0,0 +1,37 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complex) {
+        this.real += complex.real;
+        this.imaginary += complex.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complex) {
+        this.real -= complex.real;
+        this.imaginary -= complex.imaginary;
+    }
+}
