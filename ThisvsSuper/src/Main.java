651ab14bcf3fe358a678b79b1a5e0d7e86b5d7f0
skysaver00 <skysaver00@naver.com>
2021-02-16 18:18:18 +0900

    2021-02-16

---
+++ ThisvsSuper/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    //this, super의 차이는 무엇인가?
+
+    //super는 parent class의 variable과 method에 접근하기 위한 방법이다.
+    //this는 현재 class의 variable과 method에 접근하기 위한 것이다.
+
+    //두개 모두 이용 가능하지만, static한 곳은 이용하지 못한다. 이는 나중에 다룰 예정이다.
+
+    //따라서 this -> 현재 variable을 가져오기 위한것.
+    //super -> parent variable을 가져오기 위한것.
+
+    //parent가 되는 class내에서는 parent내의 variable, method(get, set)등을
+    //this로 가져 오지만,
+
+    //extends로 가지고 온 class에서 extend에 해당하는 variable를 이용하려면
+    //super()가 피룡하다.
+}
