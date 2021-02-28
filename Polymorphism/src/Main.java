ba533d0482e1e3c084373659ff0f9498ceefc799
skysaver00 <skysaver00@naver.com>
2021-02-28 23:16:01 +0900

    2021-02-28

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -20,7 +20,46 @@ class Jaws extends Movie {
     }
 }
 
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
 
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over the planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try to escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "I am your father";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    //플롯은 없다. 플롯이 없어서 Polymorphism을 이해할 수 있다.
+}
 
 public class Main {
     public static void main(String[] args) {
