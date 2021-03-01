eb2a873e86b2b983ef4f2710cd74727821112bf0
skysaver00 <skysaver00@naver.com>
2021-03-02 00:04:37 +0900

    2021-03-02

--- MasterChallange/src/Hamburger.java
+++ MasterChallange/src/Hamburger.java
@@ -13,23 +13,43 @@ public class Hamburger {
     private String addition4Name;
     private double addition4Price;
 
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+
+        System.out.println(this.name + " hamburger on a " + this.breadRollType
+        + " roll with" + this.meat + ", price is " + this.price);
+    }
+
     public void addHamburgerAddition1(String name, int price) {
         this.addition1Name = name;
         this.addition1Price = price;
+
+        System.out.println("Added " + this.name + "for an extra " + this.price);
     }
 
     public void addHamburgerAddition2(String name, int price) {
         this.addition2Name = name;
         this.addition2Price = price;
+
+        System.out.println("Added " + this.name + "for an extra " + this.price);
     }
 
     public void addHamburgerAddition3(String name, int price) {
         this.addition3Name = name;
         this.addition3Price = price;
+
+        System.out.println("Added " + this.name + "for an extra " + this.price);
     }
 
     public void addHamburgerAddition4(String name, int price) {
         this.addition4Name = name;
         this.addition4Price = price;
+
+        System.out.println("Added " + this.name + "for an extra " + this.price);
     }
+
+    public double
 }
