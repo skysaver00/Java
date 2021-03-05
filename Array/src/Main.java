fad7adc152464822a458ab328e2af59bc453e961
skysaver00 <skysaver00@naver.com>
2021-03-05 21:29:35 +0900

    2021-03-05

---
+++ Array/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+    }
+}
