b4bc54c0e9ff2281c50a8820714dca7720695d4c
skysaver00 <skysaver00@naver.com>
2021-03-05 21:56:56 +0900

    2021-03-05

---
+++ ArrayExercise/src/SortedArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SortedArray {
+    public static int[] getIntegers(int[] array) {
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++) {
+            array[i] = scan.nextInt();
+        }
+
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for(int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+        while(flag) {
+            flag = false;
+            for(int i = 0; i < sortedArray.length - 1; i++) {
+                if(sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+
+                    flag = true;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+}
