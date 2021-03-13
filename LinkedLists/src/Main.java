fc6a877de0269e6fd20a601240fd623ba01d68cb
skysaver00 <skysaver00@naver.com>
2021-03-13 18:20:54 +0900

    2021-03-13

---
+++ LinkedLists/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+    }
+}
