b838c1c44bdb9b299688d758053c321e0d4c90d9
skysaver00 <skysaver00@naver.com>
2021-03-14 20:18:51 +0900

    2021-03-14

---
+++ LinkedLists/src/Demo.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Demo {
+    //psvm을 치고 탭하면 새로 만들어진다.
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+
+        while(i.hasNext()) {
+            System.out.println("Now Visiting" + i.next());
+        }
+        System.out.println("================================");
+    }
+}
--- LinkedLists/src/Main.java
+++ LinkedLists/src/Main.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Customer customer = new Customer("Tim", 54.96);
         Customer anotherCustomer;
         anotherCustomer = customer; //메모리 주소는 같은 곳을 가르키기 때문에 바뀌지 않는다.
@@ -32,5 +32,121 @@ public class Main {
         //도중에 데이터를 고쳐서 다른 주소를 가르킬 수 있다. 그럼 더이상 연걸이 끊긴곳은 어떻게 하느냐?
         //Garbage Collection이 알아서 해준다. 쓰레기값이 돼버린 데이터는 바로 Garbage Collection이 알아서 해준다.
 
+    }*/
+
+    //psvm을 치고 탭하면 새로 만들어진다.
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+
+        /*placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+        printList(placesToVisit);
+
+        placesToVisit.add(1, "Alice Springs");
+        printList(placesToVisit);
+
+        placesToVisit.remove(5);
+        printList(placesToVisit);*/
+
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
+        printList(placesToVisit);
+
+        addInOrder(placesToVisit, "Alice Springs");
+        addInOrder(placesToVisit, "Darwin");
+        printList(placesToVisit);
     }
+
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+
+        while(i.hasNext()) {
+            System.out.println("Now Visiting " + i.next());
+        }
+        System.out.println("================================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while(stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+
+            if(comparison == 0) {
+                //equal, do not add
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if(comparison > 0) {
+                //new City should appear before this one
+                //Brisbane -> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if(comparison < 0) {
+                //move on next city
+
+            }
+        }
+
+        stringListIterator.add(newCity);
+        return true;
+    }
+
+    private static void visit(LinkedList cities) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if(cities.isEmpty()) {
+            System.out.println("No cities in the itenerary");
+            return;
+        } else {
+            System.out.println("Now visiting " + listIterator.next());
+        }
+
+        while(!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch(action) {
+                case 0:
+                    System.out.println("Holiday (Vacation) over");
+                    quit = true;
+                    break;
+
+                case 1:
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the list.");
+                    }
+                    break;
+
+                case 2:
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list.");
+                    }
+                    break;
+
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static
 }
