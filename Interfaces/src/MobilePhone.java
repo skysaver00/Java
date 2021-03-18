73ac38869ca902e1cdb6e5271936ae56329a410e
skysaver00 <skysaver00@naver.com>
2021-03-18 16:04:21 +0900

    2021-03-18

---
+++ Interfaces/src/MobilePhone.java
@@ -0,0 +1,44 @@
+public class MobilePhone implements ITelephone{
+    //이제 implement 해야한다.
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn;
+
+    public MobilePhone(int myNumber) {
+        isOn = false;
+        this.myNumber = myNumber;
+    }
+
+    @Override //이렇게 오버라이드 된다.
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Mobile Phone powered up!");
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        System.out.println("Now Ringing " + phoneNumber + " on mobile phone.");
+    }
+
+    @Override
+    public void answer() {
+        if(isRinging) {
+            System.out.println("Answering the mobile phone");
+            isRinging = false;
+        }
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if(phoneNumber == myNumber && isOn) {
+            isRinging = true;
+            System.out.println("Ring ring");
+        } else isRinging = false;
+        return false;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
+}
