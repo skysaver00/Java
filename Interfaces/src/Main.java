a7040eb38f7c782563c8399ea80c5183dd30afa4
skysaver00 <skysaver00@naver.com>
2021-03-18 16:00:40 +0900

    2021-03-18

--- Interfaces/src/Main.java
+++ Interfaces/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ITelephone = timsPhone;
+        ITelephone timsPhone;
         timsPhone = new DeskPhone(1234567890);
         timsPhone.powerOn();
     }
