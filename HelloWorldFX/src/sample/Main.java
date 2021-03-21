3fd7eb1bc5593b71cfc133454a16ea70702c645c
skysaver00 <skysaver00@naver.com>
2021-03-21 18:15:00 +0900

    2021-03-21 JavaFX는 버린다

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -2,16 +2,28 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX!");
+        root.getChildren().add(greeting);
+
+        primaryStage.setTitle("Hello JavaFX!!!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
