40781d3a07be174cbc2ff656216aee822036e750
skysaver00 <skysaver00@naver.com>
2021-07-01 00:11:14 +0900

    2021-07-01

--- DataEngineer_Java_1/src/Test.java
+++ DataEngineer_Java_1/src/Test.java
@@ -1,4 +1,19 @@
-import java.util.Scanner;
+//191페이지 예제
+/*import java.util.Scanner;
+
+public class Test {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.printf("a * 3 = %d\n", a * 3);
+        System.out.println("a / 2 = " + (a / 2));
+        System.out.print("a - 1 = " + (a - 1));
+        scan.close();
+    }
+}*/
+
+//195페이지 3번
+/*import java.util.Scanner;
 
 public class Test {
     public static void main(String args[]) {
@@ -9,4 +24,14 @@ public class Test {
 
         scan.close();
     }
-}
+}*/
+
+//197페이지 8번
+/*public class Test {
+    public static void main(String args[]) {
+        int a = 12, b = 5, sum = 2; //먼저 선언
+        b *= a /= 4; //a /= 4 -> 3되고 b * a -> 5 * 3 - > 15 근데 아무렇게나 해도 상관 없음.
+        sum += ++a * b-- / 4; //++a먼저 -> a = 4, b--니까 나중에 b = 15, sum += 4 * 15 / 4니까, 산술 먼저 -> 15를 sum에 추가 -> 2 + 15 -> 17
+        System.out.printf("%d", sum); //17을 출력할 것이다.
+    }
+}*/
