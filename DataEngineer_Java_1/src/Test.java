f4cbab611d3bf4063d44fd8dcbf38410355ff41e
skysaver00 <skysaver00@naver.com>
2021-07-01 23:17:12 +0900

    2021-07-01

--- DataEngineer_Java_1/src/Test.java
+++ DataEngineer_Java_1/src/Test.java
@@ -46,3 +46,22 @@ public class Test {
         System.out.printf("%d", c); //14를 출력
     }
 }*/
+
+//201페이지 예제
+public class Test {
+    public static void main(String args[]) {
+        String str = "agile";
+        int x[] = {1, 2, 3, 4, 5};
+        char y[] = new char[5];
+        int i = 0;
+        while(i < str.length()) {
+            y[i] = str.charAt(i);
+            i++;
+        }
+        for(int p : x) {
+            i--;
+            System.out.print(y[i]);
+            System.out.print(p + " ");
+        }
+    }
+}
