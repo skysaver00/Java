348386e1166044b9a1ce59592bf1a73847e7f023
skysaver00 <skysaver00@naver.com>
2021-07-03 22:54:17 +0900

    2021-07-03

--- DataEngineer_Java_1/src/Test.java
+++ DataEngineer_Java_1/src/Test.java
@@ -184,3 +184,20 @@ public class Test {
         }
     }
 }*/
+
+//225페이지 예제
+class ClassA {
+    int a = 10;
+    int funcAdd(int x, int y) {
+        return x + y + a;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 3, y = 6, r;
+        ClassA cal = new ClassA();
+        r = cal.funcAdd(x, y);
+        System.out.println(r);
+    }
+}
